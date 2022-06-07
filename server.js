71cbaf86efce274b5893844400fabb41a1ed0d4b
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-06-07 16:49:13 +0100

    working dogbreed api

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+const dogBreed = {
+    'german sheperd':{
+        'breed': 'German Sheperd',
+        'origin': 'Germany',
+        'pure': true,
+        'scientist': 'Unknown'
+    },
+
+    'husky':{
+        'breed': 'Australian Husky',
+        'origin': 'Australia',
+        'pure': true,
+        'scientist': 'Unknown'
+    },
+    
+    'unknown':{
+        'breed': 'Unknown',
+        'origin': 'Unknown',
+        'pure': 'Unknown',
+        'scientist': 'Unknown'
+    } 
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:breed', (req, res) =>{
+    const breedName = req.params.breed.toLowerCase()
+    if( dogBreed[breedName] ){
+        res.json(dogBreed[breedName])
+    }else{
+        res.json(dogBreed['unknown'])
+    }
+    
+})
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Live on port ${PORT}! Better go catch it!`)
+})
